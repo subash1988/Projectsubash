46d944b4efac89abf39fbdd3f72027c964fb6d92
subash1988 <subash021088india@gmail.com>
2021-12-06 19:47:28 +0530

    i am added mul method

--- src/org/Gitpush/Employee.java
+++ src/org/Gitpush/Employee.java
@@ -8,5 +8,8 @@ public class Employee {
 	public static void main(String[] args) {
 		Employee e = new Employee();
 		e.add(10, 20);	}
-
+	public void mul(int a, int b) {
+		int mul = a * b;
+		System.out.println(mul);
+	}
 }
