ede4d6ca03f73a9505de9149bea381b39bee927f
subash1988 <subash021088india@gmail.com>
2021-12-06 15:51:49 +0530

    task done

---
+++ src/org/Gitpush/Employee.java
@@ -0,0 +1,12 @@
+package org.Gitpush;
+
+public class Employee {
+	public void add(int a, int b) {
+		int sum = a + b;
+		System.out.println(sum);
+	}
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.add(10, 20);	}
+
+}
